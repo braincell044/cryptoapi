34e7f766666b9108d2fd0aa21e4877dc2fee945a
braincell044 <apple@APPLEs-MacBook-Pro.local>
2025-02-21 00:07:42 +0100

    Fix: Removed yarn.lock for Render deployment

--- app.js
+++ app.js
@@ -36,8 +36,8 @@ mongoose.connect(process.env.MONGO_URI, {
 
 
 app.get('/', (req, res) => res.send('Hello, MongoDB Atlas!'));
-app.listen(process.env.PORT || 5050, () => 
-  console.log(`🚀 Server running on port ${process.env.PORT || 5000}`)
+app.listen(process.env.PORT || 53500, () => 
+  console.log(`🚀 Server running on port ${process.env.PORT || 53500}`)
 );
 
 
